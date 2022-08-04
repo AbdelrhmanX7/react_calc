fdabeae7217c8bd30e800d452a0a6a007540760d
AbdelrhmanX7 <abdelrhman.slim26@gmail.com>
2022-08-04 20:46:46 +0200

    Updates

---
+++ precache-manifest.7d76f1a410c3e04cd2e16bebb0c848d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34714c09bde70338708c",
+    "url": "/react_calc/static/css/main.b0c59d54.chunk.css"
+  },
+  {
+    "revision": "34714c09bde70338708c",
+    "url": "/react_calc/static/js/main.34714c09.chunk.js"
+  },
+  {
+    "revision": "42809b9c2dfa35feba2d",
+    "url": "/react_calc/static/js/1.42809b9c.chunk.js"
+  },
+  {
+    "revision": "76f3089a3c35ce9903d4",
+    "url": "/react_calc/static/js/runtime~main.76f3089a.js"
+  },
+  {
+    "revision": "ca4c71c28b6c754d6ac110025f440371",
+    "url": "/react_calc/index.html"
+  }
+];
